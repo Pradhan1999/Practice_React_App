087d2b65df80423bd282a4f54ff43c529011c6c5
deepak <pradhan171219@gmail.com>
2023-01-07 20:46:26 +0530

    update

--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -12,7 +12,7 @@ const FormContainer = (callback) => {
     const emailPattern =
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const phonePattern = /^\d{10}$/;
-    const passwordPattern = /^(?=.?[A-Z])(?=.?[a-z])(?=.*?[0-9]).{8,}$/;
+    const passwordPattern = /^[a-zA-Z](?=.?[a-z])(?=.*?[0-9]).{8,}$/;
     const spaceRemove = /\s/g;
 
     switch (name) {
